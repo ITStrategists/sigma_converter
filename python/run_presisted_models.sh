422dd0c5202249f1e229131bdb4402d17293a911
Tayyab Usman <tusman@itstrategists.com>
2021-05-14 12:46:48 +0500

    Updated the dbt profiles.

--- python/run_presisted_models.sh
+++ python/run_presisted_models.sh
@@ -74,3 +74,6 @@ dbt run --models sigma_its_sig_events_summary
 dbt run --models sigma_me_its_sig_repeat_purchase_facts
 dbt run --models sigma_me_its_sig_clean_events
 dbt run --models sigma_me_its_sig_events_summary
+dbt run --models sigma_me_its_sig_repeat_purchase_facts
+dbt run --models sigma_me_its_sig_clean_events
+dbt run --models sigma_me_its_sig_events_summary
